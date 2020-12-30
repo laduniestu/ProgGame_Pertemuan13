acba5787db894a6c688e5abe17841c1dc7158393
laduniestu <duniedun@gmail.com>
2020-12-31 05:06:20 +0700

    tugas selesai alhamdulillah

---
+++ Assets/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    // komponen GUI
+    public GameObject startPanel;
+    public int playerScore = 0;
+    public Text hitungTeks;
+    public Text hitungNyawa;
+    public int ronde = 1;
+    public GameObject rondeTeks;
+    public Text jmlRonde;
+    public Text teksJmlRonde;
+    public Text targetTeks;
+    public int tembakPerRonde = 3;
+    private int nyawa = 2;
+    public GameObject[] peluru;
+    
+    // show/hide GameObject
+    public GameObject GUITeksSkor;
+    public GameObject GUITeksNyawa;
+    public GameObject GUITargetBidikan;
+    public GameObject GUITembak;
+    public GameObject GUIAnjing;
+    public GameObject GUITeksRonde;
+    public GameObject GUIGameOverPanel;
+    public GameObject GUIStartPanel;
+    public GameObject Terrain;
+
+    // control audio
+    AudioSource audio;
+    public AudioClip[] clips;
+
+    void Awake() {
+        if(instance == null) {
+            instance = this;
+        }
+    }
+}
